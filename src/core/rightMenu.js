df8173c1702c932cfdb0579ea3ed8f29ee3748f1
刘琰 <liuy@huadi.info>
2020-11-11 15:12:27 +0800

    修改rightMenus增加行、列，删除行、列时不跟着变化的问题

--- src/core/rightMenu.js
+++ src/core/rightMenu.js
@@ -21,14 +21,15 @@ class RightMenus {
     this._ = ndata;
   }
 
-  delete(sri) {
+  delete(sri, eri) {
+    const n = eri - sri + 1;
     const ndata = [];
     for (let i = 0; i < this._.length; i += 1) {
       const v = this._[i];
       if (v.ri < sri) {
         ndata.push(v);
-      } else if (v.ri > sri) {
-        v.ri -= 1;
+      } else if (v.ri > eri) {
+        v.ri -= n;
         ndata.push(v);
       }
     }
@@ -49,14 +50,15 @@ class RightMenus {
     this._ = ndata;
   }
 
-  deleteColumn(sci) {
+  deleteColumn(sci, eci) {
+    const n = eci - sci + 1;
     const ndata = [];
     for (let i = 0; i < this._.length; i += 1) {
       const v = this._[i];
       if (v.ci < sci) {
         ndata.push(v);
-      } else if (v.ci > sci) {
-        v.ci -= 1;
+      } else if (v.ci > eci) {
+        v.ci -= n;
         ndata.push(v);
       }
     }
